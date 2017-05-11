8e54ba0d004c5c6b6fc201666ad14a995daf2094
poulpe <hugouf@hotmail.fr>
2017-05-11 13:54:45 +0200

    added motion test

--- src/tests/index.js
+++ src/tests/index.js
@@ -3,6 +3,7 @@
 var AppMini = require("./minimal");
 var AppProj = require("./projection");
 var AppCam = require("./camera");
+var AppMotion = require("./motion");
 var App1 = require("./test1");
 var App2 = require("./test2");
 var App3 = require("./test3");
@@ -11,6 +12,7 @@ module.exports = {
     AppMini: AppMini,
     AppProj: AppProj,
     AppCam: AppCam,
+    AppMotion: AppMotion,
     App1: App1,
     App2: App2,
     App3: App3,
---
+++ src/tests/motion.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var wiggle = require('wiggle');
+
+var App = function () {
+    this.context = new wiggle.context.Context('appcanvas');
+    this.moving = false;
+};
+
+App.prototype.setup = function () {
+    this.context.resizeIfNeeded();
+
+    this.shader = new wiggle.Shader();
+    this.shader.init(
+        require('./shaders/v_cam.glsl'),
+        require('./shaders/f_mini.glsl')
+    );
+    this.shader.use();
+
+    this.projection = new wiggle.Projection('uProjMat');
+    this.projection.ortho();
+
+    this.camera = new wiggle.Camera('uViewMat');
+
+    this.vertArr = new wiggle.buffers.BufferArray(
+        Object.keys(this.shader.attributes)
+    );
+    this.vertArr.buff('aVertexPosition').add([
+        [0, 1], [0, 0], [1, 0],
+    ]);
+    this.vertArr.commit();
+
+    this.mot1 = new wiggle.motion.Motion('mot1', this.mot1Cb.bind(this));
+    this.mot2 = new wiggle.motion.Motion('mot2', this.mot2Cb.bind(this));
+    this.mot3 = new wiggle.motion.Motion(
+        'mot3', wiggle.motion.slowdownWrapper(this.mot3Cb.bind(this), 2000)
+    );
+    this.mot4 = new wiggle.motion.Motion(
+        'mot4', wiggle.motion.slowdownWrapper(this.mot4Cb.bind(this), 5000)
+    );
+
+    this.mot1.nowGetter = this.nowGetter.bind(this);
+    this.mot2.nowGetter = this.nowGetter.bind(this);
+    this.mot3.nowGetter = this.nowGetter.bind(this);
+    this.mot4.nowGetter = this.nowGetter.bind(this);
+
+    this.mot3.running = true;
+    this.mot4.running = true;
+
+    this.next();
+};
+
+App.prototype.next = function () {
+    window.requestAnimationFrame(this.step.bind(this));
+};
+
+App.prototype.nowGetter = function () {
+    return this.nowTs;
+};
+
+App.prototype.mot1Cb = function (name, idx, currentTs, startTs, previousTs) {
+    this.camera.node.transform.rZ -= 1 * (currentTs - previousTs) / 1000;
+};
+
+App.prototype.mot2Cb = function (name, idx, currentTs, startTs, previousTs) {
+    this.camera.node.transform.rZ += 1 * (currentTs - previousTs) / 1000;
+};
+
+App.prototype.mot3Cb = function (name, idx, currentTs, startTs, previousTs) {
+    this.mot1.running = true;
+    this.mot2.running = false;
+};
+App.prototype.mot4Cb = function (name, idx, currentTs, startTs, previousTs) {
+    this.mot1.running = false;
+    this.mot2.running = true;
+};
+
+App.prototype.teardown = function () {
+    this.stop = true;
+};
+
+App.prototype.step = function (nowTs) {
+    if (this.stop) {
+        return;
+    }
+    this.nowTs = nowTs;
+
+    this.camera.node.transform.tY = 2;
+    this.camera.node.transform.refresh();
+
+    this.mot1.tick();
+    this.mot2.tick();
+    this.mot3.tick();
+    this.mot4.tick();
+
+    this.draw();
+
+    this.next();
+};
+
+App.prototype.draw = function () {
+    this.context.clear();
+
+    this.shader.use();
+
+    this.camera.expose();
+    this.projection.expose();
+    this.vertArr.draw();
+};
+
+module.exports = App;
